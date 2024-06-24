085abdb5bcb2f9216c54e7eff2e3bea6bcac1cad
Manish Patel <patelm.2280@gmail.com>
2024-06-24 11:30:10 +0530

    gem updated

--- hello_gem/lib/hello_gem.rb
+++ hello_gem/lib/hello_gem.rb
@@ -5,13 +5,4 @@ require_relative "hello_gem/version"
 module HelloGem
   class Error < StandardError; end
   # Your code goes here...
-  def self.hello
-    puts "Hello from my gem!"
-  end
-
-  def self.hello_from_gem(post)
-    # Example functionality
-    post.update(title: post.title)
-  end
-
 end
