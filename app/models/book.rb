b2c9eb3256c3839ceff0a9c98e2724d9906e45af
Manish Patel <patelm.2280@gmail.com>
2024-07-23 18:07:11 +0530

    metaprogramming to validate field

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
     has_many :authors, dependent: :destroy
     acts_as_paranoid
+
+    #Metaprogramming
+    %w(title content).each do |attribute|
+        validates attribute, presence: true
+    end
 end
