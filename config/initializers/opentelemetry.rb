b390f2d0582bc07a30c1b38fa995855135a568ed
Manish Patel <patelm.2280@gmail.com>
2024-09-09 18:54:31 +0530

    OpenTelemetry implemented

---
+++ config/initializers/opentelemetry.rb
@@ -0,0 +1,8 @@
+# config/initializers/opentelemetry.rb
+require 'opentelemetry/sdk'
+require 'opentelemetry/instrumentation/all'
+OpenTelemetry::SDK.configure do |c|
+  c.service_name = 'blog-ruby'
+  c.use_all() # enables all instrumentation!
+end
+
